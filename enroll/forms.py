36653f255a6498475b13f40747c16a73f24c486a
Majumder99 <souravmajumderk99@gmail.com>
2024-07-14 18:06:21 +0600

    insert data into db

---
+++ enroll/forms.py
@@ -0,0 +1,14 @@
+from django import forms
+from .models import User
+from django.core import validators
+
+class StudentRegistration(forms.ModelForm):
+    class Meta:
+        model = User
+        fields = ['name', 'email', 'password']
+        labels = {'name':'Enter Name', 'email':'Enter Email', 'password':'Enter Password'}
+        widgets = {
+            'name': forms.TextInput(attrs={'class':'form-control'}),
+            'email': forms.EmailInput(attrs={'class':'form-control'}),
+            'password': forms.PasswordInput(attrs={'class':'form-control'}),
+        }
