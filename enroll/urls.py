33115bb79b36c5809eaa030689120fcee8dee1e7
Majumder99 <souravmajumderk99@gmail.com>
2024-07-18 21:40:08 +0600

    done the crude app

--- enroll/urls.py
+++ enroll/urls.py
@@ -2,5 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('view', views.add_show),
+    path('view', views.add_show, name="add_show"),
+    path('delete/<int:id>', views.delete_data, name="delete_data"),
+    path('edit/<int:id>', views.edit_data, name="edit_data"),
 ]
--- enroll/views.py
+++ enroll/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import redirect, render
 from .forms import StudentRegistration
 from .models import User
 
@@ -12,8 +12,30 @@ def add_show(request):
             pw = fm.cleaned_data['password']
             reg = User(name=nm, email=em, password=pw)
             reg.save()
+            return redirect('add_show')  # Redirect after successful form submission
     else:
         fm = StudentRegistration()
     stu = User.objects.all()
     return render(request, 'enroll/addandshow.html', {'form':fm, 'students': stu})
 
+# This function will delete
+def delete_data(request, id):
+    if request.method == "POST":
+        # id = request.POST.get('id')
+        pi = User.objects.get(pk=id)
+        pi.delete()
+        return redirect('add_show')
+    return render
+
+
+def edit_data(request, id):
+    if request.method == "POST":
+        pi = User.objects.get(pk=id)
+        fm = StudentRegistration(request.POST, instance=pi)
+        if fm.is_valid():
+            fm.save()
+            return redirect('add_show')
+    else:
+        pi = User.objects.get(pk=id)
+        fm = StudentRegistration(instance=pi)
+    return render(request, 'enroll/updatestudent.html', {'form':fm})
